7998f7752dce13e33fb3dd0b1b6ea0ad5bb49aa7
Tom Werner <hello@tomwerner.me.uk>
2015-10-18 22:21:22 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'zohoautomate',
+  packages = ['zohoautomate'], # this must be the same as the name above
+  version = '0.1',
+  description = '''A library to do basic operations in Zoho Mail, as they don't provide an API.''',
+  author = 'Tom Werner',
+  author_email = 'hello@tomwerner.me.uk',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
---
+++ zohoautomate/zohoautomate.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+class ZohoAutomater:
+
+    def __init__(self, username, password):
+        self.browser_handle = self.load_zoho(username, password)
+
+    def load_zoho(self, username, password):
+        browser = webdriver.Firefox()
+        browser.get('https://www.zoho.com/mail/login.html')
+
+        browser.switch_to.frame("zohoiam")
+
+        userInputElement = browser.find_element_by_name("lid")
+        userInputElement.send_keys(username)
+        pwdInputElement = browser.find_element_by_name("pwd")
+        pwdInputElement.send_keys(password)
+        pwdInputElement.submit()
+
+        time.sleep(15)
+
+        return browser
+
+    def check_contact_is_present(self, email):
+        pass
+
+    def add_contact_to_group(self, email, groupname):
+        browser = self.browser_handle
+
+        browser.get('https://contacts.zoho.com/home.zc')
+
+        time.sleep(15)
+
+        try:
+            cancel_wizard = browser.find_element_by_link_text('Skip this step »')
+        except NoSuchElementException as exc:
+            pass
+        else:
+            cancel_wizard.click()
+
+        contact_cbox = browser.find_element_by_xpath('''//dt[@title="{0}"]'''.format(email))
+        contact_cbox.click()
+
+        addtolist_button = browser.find_element_by_id('categoriesSpan')
+        addtolist_button.click()
+
+        add_to_group_item = browser.find_element_by_xpath('''//div[@categoryname="{0}"]'''.format(groupname))
+        add_to_group_item.click()
+
+
+    def add_contact_to_zoho(self, firstname, lastname, email):
+        browser = self.browser_handle
+
+        browser.get('https://contacts.zoho.com/home.zc')
+
+        time.sleep(15)
+
+        try:
+            cancel_wizard = browser.find_element_by_link_text('Skip this step »')
+        except NoSuchElementException as exc:
+            pass
+        else:
+            cancel_wizard.click()
+
+        addnew_button = browser.find_element_by_css_selector('.addnew > div:nth-child(1)')
+        addnew_button.click()
+
+        fname = browser.find_element_by_name('addFname')
+        fname.send_keys(firstname)
+        lname = browser.find_element_by_name('addLname')
+        lname.send_keys(lastname)
+        emailinput = browser.find_element_by_name('pmail')
+        emailinput.send_keys(email)
+
+        save_button = browser.find_element_by_css_selector('.contadd > div:nth-child(6) > div:nth-child(2) > div:nth-child(1) > span:nth-child(2)')
+        save_button.click()
+
+        time.sleep(5)
