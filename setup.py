90644f0dd2e401dcc8f2730791c6ad177d02b833
Tom Werner <hello@tomwerner.me.uk>
2015-10-19 10:59:17 +0100

    updated project metadata

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 setup(
   name = 'zohoautomate',
   packages = ['zohoautomate'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.0.1',
   description = '''A library to do basic operations in Zoho Mail, as they don't provide an API.''',
   author = 'Tom Werner',
-  author_email = 'hello@tomwerner.me.uk',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
+  author_email = 'hello.github@tomwerner.me.uk',
+  url = 'https://github.com/tomwerneruk/zohoautomate', # use the URL to the github repo
+  download_url = 'https://github.com/tomwerneruk/zohoautomate/tarball/v0.0.2', # I'll explain this in a second
+  keywords = ['zoho', 'api', 'automation', 'mail'], # arbitrary keywords
+  install_requires =[
+    'selenium',
+  ]
 )
